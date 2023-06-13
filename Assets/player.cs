7566ca541b46bb3cdb57da483c84082cf5935958
matuhiro97 <115542804+matuhiro97@users.noreply.github.com>
2023-06-13 20:02:02 +0900

    playerとスクリプトを追加した
    上下左右に動く仮のスクリプトを追加した。
    player（仮）を追加した。これらにはRIぽじとｒｙ２ｄとboxcolideerを与えている。

---
+++ Assets/player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private float speed = 0.05f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Vector2 position = transform.position;
+
+        if (Input.GetKey("left"))
+        {
+            position.x -= speed;
+        }
+        else if (Input.GetKey("right"))
+        {
+            position.x += speed;
+        }
+
+       
+        else if (Input.GetKey("up"))
+        {
+            position.y += speed;
+        }
+        else if (Input.GetKey("down"))
+        {
+            position.y -= speed;
+        }
+
+
+       
+        transform.position = position;
+    }
+}
