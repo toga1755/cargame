529f03861cb673f4a99a3de919fb0ee2fa34e12a
tomooooki <133968509+tomooooki@users.noreply.github.com>
2023-06-22 20:54:05 +0900

    車追加

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,34 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    private float speed = 0.05f;
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 
-    void Start()
-    {
-    }
+public class player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private int moveSpeed;
+    [SerializeField] private int jumpForce;
+
+    private bool isJumping = false;
 
     void Update()
     {
-        Vector2 position = transform.position;
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping && !(rb.velocity.y < -0.5f))
+        {
+            Jump();
+        }
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y);
+    }
+    void Jump()
+    {
+        isJumping = true;
 
-        if (Input.GetKey("left"))
-        {
-            position.x -= speed;
-        }
-        else if (Input.GetKey("right"))
-        {
-            position.x += speed;
-        }
-        else if (Input.GetKey("up"))
-        {
-            position.y += speed;
-        }
-        
-
-        transform.position = position;
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
-
+     void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Stage"))
+        {
+            isJumping = false;
+        }
+    }
 }
