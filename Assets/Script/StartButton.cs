c3f07a9e6900e830cecd37b35b207884aab5989c
toga1755 <84073607+toga1755@users.noreply.github.com>
2023-07-07 14:09:03 +0900

    コース選択完成
    コース選択ができるようになった

--- Assets/Script/StartButton.cs
+++ Assets/Script/StartButton.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
+    private int _selected = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _selected = PlayerPrefs.GetInt("CourseSelect", 0);        
     }
 
     // Update is called once per frame
@@ -19,6 +21,14 @@ public class StartButton : MonoBehaviour
 
     public void OnClickStartButton()
     {
-        SceneManager.LoadScene("1_courseScene");
+        if(_selected == 0){
+            SceneManager.LoadScene("1_courseScene");
+        }
+        if(_selected == 1){
+            SceneManager.LoadScene("2_courseScene");
+        }
+        if(_selected == 2){
+            SceneManager.LoadScene("3_courseScene");
+        }
     }
 }
